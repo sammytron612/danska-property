a55429ea8f5a1db08ed396820d9b2a46b812d58e
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2022-09-30 12:36:17 +0100

    ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,8 @@
 
 <head>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 @if(!isset($title))
     @php $title = "Danska Property Services";
