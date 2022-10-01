5d50c9234a21c3717a77c39ca7a6fefd569a4b4b
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2022-10-01 14:46:18 +0100

    ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -159,7 +159,7 @@
         <div class="col-lg-3 col-md-6 footer-links">
           <h4>Follow Us</h4>
           <div class="social-links d-flex">
-            <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+            <a href="https://twitter.com/danskaprop2022" class="twitter"><i class="bi bi-twitter"></i></a>
             <a href="https://www.facebook.com/profile.php?id=100086131102819" class="facebook"><i class="bi bi-facebook"></i></a>
           </div>
         </div>
