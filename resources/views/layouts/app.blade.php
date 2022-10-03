522ea28f0b0324cf5df0845dd9d8727f91389e54
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2022-10-03 12:47:24 +0100

    ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,8 @@
 
 <head>
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 @if(isset($noIndex))
     <meta name="googlebot" content="noindex">
 @endif
